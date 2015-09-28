2107c36a3e819fc226bb0f5f0b29ca01e7027a72
victor <victor.vicente@attime.es>
2015-09-28 13:15:14 +0200

    Inicio de Repositorio

---
+++ holamundo/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hola Mundo!</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	hola señor mundo!
+
+	<br>
+	<?php
+		echo "<h1>Hola mundo!</h1>";
+
+		echo "bla blqvdgadfknbnsdfjkm
+		dfgsdfbgadsghsa
+		adergadfga";
+
+	?>
+</body>
+</html>
---
+++ probandoDebug/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TIPO ENTERO VARIABLES PHP</title>
+</head>
+<body>
+	<?php
+		$numero=13;
+		echo $numero;
+	?>
+</body>
+</html>
