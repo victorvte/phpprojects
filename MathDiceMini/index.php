232fb951be0dada8ff0d6df86e938b5a1e07a4b0
victorvte <victorvte94@gmail.com>
2015-10-15 20:43:56 +0000

    terminado randomize dado y añadido padding

--- MathDiceMini/index.php
+++ MathDiceMini/index.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="/css/estilo.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
 
@@ -49,7 +49,7 @@
     <div class="col-sm-2"> </div>
     <div class="col-sm-4"> </div>
   </div>
-  <div class="row">
+  <div class="row" id="numRandom">
     <div class="col-sm-2"> Tu dado es: </div>
     <div class="col-sm-4"> <img src="imagenes/dado<?= $numeroDado ?>.png" class="img-responsive"></div>
   </div>
